3472d0b3b23402d1fe84d5d49f7b905289758cf6
Axcis380 <anas.baidouri@gmail.com>
2023-06-15 13:08:12 -0700

    fix tsk

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Taille d'un char : %lu octet(s)\n", sizeof(char));
-    printf("Taille d'un int : %lu octet(s)\n", sizeof(int));
-    printf("Taille d'un double : %lu octet(s)\n", sizeof(double));
-    printf("Taille d'un long int : %lu octet(s)\n", sizeof(long int));
-    printf("Taille d'un long long int : %lu octet(s)\n", sizeof(long long int));
-    printf("Taille d'un float : %lu octet(s)\n", sizeof(float));
-    return 0;
-}
+/**
+ * main - Entry point
+ *
+ * Description: using sizeof to print the size of various types.
+ *
+ * Return: Always 0 (Succesful)
+*/
 
+int main(void)
+{
+
+	/* sizeof evaluates the size of a variable */
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of an double: %lu byte(s)\n", sizeof(double));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+
+	return (0);
+
+}
