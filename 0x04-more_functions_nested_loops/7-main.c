1ae6eb65df8f53ec155a0600ad7ab59994835989
Axcis380 <anas.baidouri@gmail.com>
2023-06-22 08:04:23 -0700

    hey world

--- 0x04-more_functions_nested_loops/7-main.c
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -1,4 +1,4 @@
-#include "../main.h"
+#include "main.h"
 
 /**
  * main - check the code for ALX School students
@@ -8,8 +8,9 @@
 
 int main(void)
 {
-	print_square(2);
-	print_square(10);
-	print_square(0);
+	print_diagonal(0);
+	print_diagonal(2);
+	print_diagonal(10);
+	print_diagonal(-4);
 	return (0);
 }
--- 0x04-more_functions_nested_loops/8-main.c
+++ 0x04-more_functions_nested_loops/8-main.c
@@ -1,4 +1,4 @@
-#include "../main.h"
+#include "main.h"
 
 /**
  * main - check the code for ALX School students
@@ -8,9 +8,8 @@
 
 int main(void)
 {
-	print_diagonal(0);
-	print_diagonal(2);
-	print_diagonal(10);
-	print_diagonal(-4);
+	print_square(2);
+	print_square(10);
+	print_square(0);
 	return (0);
 }
