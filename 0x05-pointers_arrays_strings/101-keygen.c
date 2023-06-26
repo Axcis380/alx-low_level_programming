d5ec01b7978d78ab72e6359f202073cd29460d67
Axcis380 <anas.baidouri@gmail.com>
2023-06-26 03:34:07 -0700

    fix hack

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -5,53 +5,52 @@
 #define PASSWORD_LENGTH 13
 
 /**
- * main - Generate random valid passwords for the
- *program 101-crackme.
- * Return: Alwas  0. 
+ * main - Generate random valid passwords for the program 101-crackme.
+ * Return: Always 0.
  */
 int main(void)
 {
-	char password[84];
-	int index = 0,diff_half1, diff_half2;
-	srand(time(0));
-	while (sum< 2772)
-{
-	password[index] = 33 + rand() % 94;
-	sum += password[index++]
-}
-password[index] = '\0';
-if (sum != 2772)
-	diff_half1 = (sum - 2772) / 2;
-	diff_half2 = (sum - 2772) / 2;
+    char password[PASSWORD_LENGTH + 1];
+    int index = 0, sum = 0, diff_half1, diff_half2;
 
-	if ((sum - 2772) % 2 != 0)
-	diff_half1++;
+    srand(time(0));
 
-	for (index = 0; password[index];index++)
-	{
-		if (password[index] >= (33 + diff_half1))
-		{
-			password[index] -= diff-half1;
-			break;
-		}
-	}
-for (index = 0; password[index]; index++)
-{
-	if (password[index] >= (33 + diff_half2))
-	{
-password[inddex] -= diff_half1;
-					break;
-	}
+    while (sum < 2772)
+    {
+        password[index] = 33 + rand() % 94;
+        sum += password[index++];
+    }
+
+    password[index] = '\0';
+
+    if (sum != 2772)
+    {
+        diff_half1 = (sum - 2772) / 2;
+        diff_half2 = (sum - 2772) / 2;
+
+        if ((sum - 2772) % 2 != 0)
+            diff_half1++;
+        
+        for (index = 0; password[index]; index++)
+        {
+            if (password[index] >= (33 + diff_half1))
+            {
+                password[index] -= diff_half1;
+                break;
+            }
+        }
+
+        for (index = 0; password[index]; index++)
+        {
+            if (password[index] >= (33 + diff_half2))
+            {
+                password[index] -= diff_half2;
+                break;
+            }
+        }
+    }
+
+    printf("%s", password);
+    return (0);
 }
-	for (index = 0; password[index]; index++)
-	{
-		if (password[index] >= (33 + diff_half2))
-		{
-			password[index] -= diff_half2;
-			break;
-		}
-	}
-}
-	printf("%s", password);
-	return (0);
-}
+
