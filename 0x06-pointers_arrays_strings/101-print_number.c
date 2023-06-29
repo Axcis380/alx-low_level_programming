54e90a0dc986200c8e4b184e2785765fc4a3ed15
Axcis380 <anas.baidouri@gmail.com>
2023-06-29 16:52:55 -0700

    fix

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,24 +1,29 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer;
- * @n: integer to be printed;
+ * print_number - Prints an integer.
+ * @n: Integer to be printed.
+ *
+ * Description: The function prints the given integer @n. It handles both positive
+ * and negative numbers and uses recursion to print each digit.
  */
 void print_number(int n)
 {
-	unsigned int n1;
+    unsigned int n1;
 
-	if (n < 0)
-	{
-		n1 = -n;
-		_putchar('-');
-	} else
-	{
-		n1 = n;
-	}
+    if (n < 0)
+    {
+        n1 = -n;
+        _putchar('-');
+    }
+    else
+    {
+        n1 = n;
+    }
 
-	if (n1 / 10)
-		print_number(n1 / 10);
+    if (n1 / 10)
+        print_number(n1 / 10);
 
-	_putchar((n1 % 10) + '0');
+    _putchar((n1 % 10) + '0');
 }
+
