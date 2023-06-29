6fdcd30bc00beac3889b94c5a4d7ea7e1c6ce975
Axcis380 <anas.baidouri@gmail.com>
2023-06-29 16:58:37 -0700

    fix

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,30 +1,57 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_number - Prints an integer.
+ * base10 - Calculates the power of 10.
+ * @n: Exponent.
+ *
+ * Return: Returns 10 to the power of the exponent.
+ */
+int base10(int n)
+{
+	int base = 10;
+
+	while (n > 0)
+	{
+		base *= 10;
+		n--;
+	}
+	return (base);
+}
+
+/**
+ * print_number - Prints an integer using _putchar.
  * @n: Integer to be printed.
  *
- * Description: The function prints the given integer @n. It handles both positive
- * and negative numbers and uses recursion to print each digit.
+ * Description: The function prints the given integer @n using only the _putchar
+ * function. It handles both positive and negative numbers and uses a helper function
+ * to calculate the power of 10. The integer is printed digit by digit.
  */
 void print_number(int n)
 {
-    unsigned int n1;
+	int power;
 
-    if (n < 0)
-    {
-        n1 = -n;
-        _putchar('-');
-    }
-    else
-    {
-        n1 = n;
-    }
+	power = base10(8);
 
-    if (n1 / 10)
-        print_number(n1 / 10);
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
 
-    _putchar((n1 % 10) + '0');
+	if (n == 0)
+		_putchar('0');
+
+	else
+	{
+		while (n / power == 0)
+			power /= 10;
+
+		while (power >= 1)
+		{
+			_putchar((n / power) + '0');
+			n %= power;
+			power /= 10;
+		}
+	}
 }
 
