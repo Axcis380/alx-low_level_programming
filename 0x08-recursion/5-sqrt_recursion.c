c6ae2dab5d6a8f8f7ed538955ed088fcd6676745
Axcis380 <anas.baidouri@gmail.com>
2023-07-05 06:09:07 -0700

    hey

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,4 @@
-#include "main.h"
-
-
+#include <stdio.h>
 
 /**
  * _sqrt_recursion_wrapper - a wrapper that does the recursion bit
@@ -13,19 +11,18 @@
 */
 int _sqrt_recursion_wrapper(int n, int min, int max)
 {
-	int guess, guess_squared;
+    if (min > max)  /* If the range of values is invalid, return -1 */
+        return -1;
 
-	guess = (min + max) / 2;		/*get guess (g)*/
-	guess_squared = guess * guess;		/*square guess (sg)*/
+    long int guess = (min + max) / 2;              /* Get an estimation (guess) */
+    long int guess_squared = guess * guess;        /* Square the guess */
 
-	if (guess_squared == n)			/*if squared guess == n return guess*/
-		return (guess);
-	else if (min == max)			/*this means n doesn't have a perfect square*/
-		return (-1);
-	else if (guess_squared < n)		/*overshoot g if sg is less than the n*/
-		return (_sqrt_recursion_wrapper(n, guess + 1, max));
-	else					/*undershoot g if sg is more than the n*/
-		return (_sqrt_recursion_wrapper(n, min, guess - 1));
+    if (guess_squared == n)                         /* If the squared guess is equal to n, return the guess */
+        return guess;
+    else if (guess_squared < n)                     /* If the squared guess is less than n, recursion with a higher range */
+        return _sqrt_recursion_wrapper(n, guess + 1, max);
+    else                                            /* If the squared guess is greater than n, recursion with a lower range */
+        return _sqrt_recursion_wrapper(n, min, guess - 1);
 }
 
 /**
@@ -38,12 +35,28 @@ int _sqrt_recursion_wrapper(int n, int min, int max)
 */
 int _sqrt_recursion(int n)
 {
-	if (n == 1)		/*sqrt(1) == 1*/
-		return (1);
-	else if (n == 0)	/*sqrt(0) == 0*/
-		return (0);
-	else if (n < 0)		/*sqrt(-n) == -1 (NA)*/
-		return (-1);
-	else
-		return (_sqrt_recursion_wrapper(n, 1, n));
+    if (n < 0)       /* If n is negative, return -1 */
+        return -1;
+
+    return _sqrt_recursion_wrapper(n, 0, n); /* Call the recursion function with a valid range */
 }
+
+int main(void)
+{
+    int r;
+
+    r = _sqrt_recursion(1);
+    printf("%d\n", r);
+    r = _sqrt_recursion(1024);
+    printf("%d\n", r);
+    r = _sqrt_recursion(16);
+    printf("%d\n", r);
+    r = _sqrt_recursion(17);
+    printf("%d\n", r);
+    r = _sqrt_recursion(25);
+    printf("%d\n", r);
+    r = _sqrt_recursion(16777216);
+    printf("%d\n", r);
+    return 0;
+}
+
