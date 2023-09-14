8b855938f69df69d66a9084a4d30e7c50a5ef7f2
Axcis.380 <anas.baidouri@gmail.com>
2023-09-14 06:48:52 -0700

    hey

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -5,14 +5,16 @@
  * @head: Pointer to the head of the list
  * Return: The sum of elements in the list, or 0 if the list is empty
  */
-
 int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
+
 	while (head != NULL)
 	{
 		sum += head->n;
 		head = head->next;
 	}
+
 	return (sum);
 }
+
