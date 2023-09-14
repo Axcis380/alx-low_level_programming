bf9fa839a8ee4424cb40d2d311f409c9bbb81e16
Axcis.380 <anas.baidouri@gmail.com>
2023-09-14 06:52:55 -0700

    hey

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,72 +1,73 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * create_dnode - Creates a new doubly linked node.
- * @n: The data to be stored in the new node.
- * @prev: Pointer to the previous node.
- * @next: Pointer to the next node.
- * Return: Pointer to the newly created node.
+ * delete_dnodeint_at_index - delete node at given index
+ * @head: pointer to the list
+ * @index: given index
+ * Return: -1 or 0
  */
-
-
-dlistint_t *create_dnode(int n, dlistint_t *prev, dlistint_t *next)
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *new;
+	dlistint_t *start;
+	unsigned int i;
+	unsigned int len;
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	new->prev = prev;
-	new->next = next;
-	return (new);
+	len = len_node(head);
+	start = *head;
+
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		start = start->next;
+		free(*head);
+		*head = start;
+
+		if (start != NULL)
+			start->prev = NULL;
+
+		return (1);
+	}
+
+	for (i = 0; i <= index - 1; i++)
+	{
+		start = start->next;
+		if (!start)
+			return (-1);
+	}
+
+	if (len - 1 == index)
+	{
+		start->prev->next = NULL;
+		free(start);
+		return (1);
+	}
+
+	start->prev->next = start->next;
+	start->next->prev = start->prev;
+	free(start);
+	return (1);
 }
 
 /**
- * insert_dnodeint_at_index - Inserts a new node at a given position in a doubly linked list.
- * @h: Pointer to the head of the doubly linked list.
- * @idx: The index for insertion of the new node.
- * @n: The data for the new node.
- * Return: Address of the new node, or NULL if an error occurs.
+ * len_node - calculate list length
+ * @node: pointer to the list
+ * Return: unsigned int
  */
-
-
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+unsigned int len_node(dlistint_t **node)
 {
-	dlistint_t *curr = *h, *localPrev = NULL;
-	unsigned int count = 0;
+	unsigned int len = 0;
+	dlistint_t *start;
 
-	if (!h)
-		return (NULL);
-	if (idx == 0) /* insert at list beginning*/
+	start = *node;
+
+	while (start != NULL)
 	{
-		if (!*h)
-			*h = create_dnode(n, NULL, NULL); /*first node*/
-		else
-		{
-			(*h)->prev = create_dnode(n, NULL, *h);
-			*h = (*h)->prev;
-		}
-		return (*h);
+		len += 1;
+		start = start->next;
 	}
-	for (curr = *h; curr && (count < idx); curr = curr->next, count++)
-	{
-		localPrev = curr;
-	}
-	if ((count == idx) && (curr == NULL)) /*insert at list end*/
-	{
-		localPrev->next = create_dnode(n, localPrev, NULL);
-		return (localPrev->next);
-	}
-	if ((count < idx) && (curr == NULL))/*idx too high*/
-		return (NULL);
-	if (localPrev != NULL)
-	{       /*insert in middle of list*/
-		localPrev->next = create_dnode(n, localPrev, curr);
-		curr->prev = localPrev->next;
-		return (localPrev->next);
-	}
-	return (NULL); /*should never run*/
+
+	return (len);
 }
