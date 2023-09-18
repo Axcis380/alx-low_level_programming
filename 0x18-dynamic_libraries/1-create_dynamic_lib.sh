c2f0b5f36e07569765af9f413b90157b058f73cd
Axcis.380 <anas.baidouri@gmail.com>
2023-09-18 08:30:55 -0700

    lol

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-# Compile the C files into object files with appropriate options
 gcc -c -Wall -Werror -fpic *.c
-
-# Create the shared library by linking the compiled objects
 gcc -shared -o liball.so *.o
-
-# Clean up the object files after creating the library
 rm -f *.o
